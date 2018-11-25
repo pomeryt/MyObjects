1dfe97dbed31177b298be6fa23b55ae9faa6ed8b
Rin <pomeryt@gmail.com>
2018-11-25 20:22:53 +0900

    Remove overlap checking. fixes #11

--- src/test/java/plain/value/SyncedValueTest.java
+++ src/test/java/plain/value/SyncedValueTest.java
@@ -3,8 +3,6 @@ package plain.value;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -17,20 +15,13 @@ class SyncedValueTest {
 	@Test
 	@SuppressFBWarnings("UC_USELESS_OBJECT")
 	void test() {
-		final AtomicBoolean running = new AtomicBoolean(false);
-		final AtomicInteger overlaps = new AtomicInteger(0);
 		final List<Exception> exceptions = Collections.synchronizedList(new ArrayList<>());
 		final SyncedValue<String> fruit = new SyncedValue<>("Apple");
 		final List<Thread> threads = new ArrayList<>();
 		for (int x = 0; x < 1000; x++) {
 			threads.add(new Thread(() -> {
 				try {
-					if (running.get()) {
-						overlaps.incrementAndGet();
-					}
-					running.set(true);
 					fruit.update(fruit.value());
-					running.set(false);
 				} catch (Exception exception) {
 					exceptions.add(exception);
 				}
@@ -38,11 +29,6 @@ class SyncedValueTest {
 		}
 		threads.forEach(Thread::start);
 		
-		MatcherAssert.assertThat(
-			"Overlaps should be greater than 0.", 
-			overlaps.get(), 
-			Matchers.greaterThan(0)
-		);
 		MatcherAssert.assertThat(
 			"No exception should be thrown.",
 			exceptions.size(), 
