fd1c5afa6ff9617241781578866e94f0a4a19ab4
Rin <pomeryt@gmail.com>
2018-11-30 01:22:55 +0900

    Rewrite the test case for SyncedValue. fixes #15

--- src/test/java/plain/value/SyncedValueTest.java
+++ src/test/java/plain/value/SyncedValueTest.java
@@ -2,95 +2,61 @@ package plain.value;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
-@SuppressFBWarnings(value = "RV_RETURN_VALUE_IGNORED_NO_SIDE_EFFECT", justification = "Return values are not important to the thread-safey testing.")
+@SuppressWarnings("PMD.AvoidCatchingGenericException")
 class SyncedValueTest {
 
 	@Test
-	public void proveThreadSafetyByManipulatingNonThreadSafeList() throws InterruptedException {
-		final List<Integer> nonThreadSafeList = new ArrayList<>();
-		final EventValue<Integer> number = new SyncedValue<>(
-			new EventValue<Integer>() {
-				@Override
-				public Integer value() {
-					return nonThreadSafeList.get(nonThreadSafeList.size() - 1);
-				}
-
-				@Override
-				public void update(final Integer value) {
-					nonThreadSafeList.add(value);
-				}
-
-				@Override
-				public void addEvent(final UpdateEvent<Integer> event) {
-					nonThreadSafeList.remove(0);
-				}
+	public void allMethodsShouldBeExecutedOneByOne() throws InterruptedException {
+		final int[] incrementedAmount = new int[1];
+		final int incrementAmount = 1;
+		final EventValue<Integer> number = new SyncedValue<>(new EventValue<Integer>() {
+			@Override
+			public Integer value() {
+				incrementedAmount[0] = incrementedAmount[0] + incrementAmount;
+				return incrementAmount;
 			}
-		);
-		final int totalThreadAmount = 300;
-		final int remainingThreadAmount = 100;
-		final int addingThreadAmount = totalThreadAmount - remainingThreadAmount;
-		final int removingThreadAmount = totalThreadAmount - addingThreadAmount;
-		final CountDownLatch latch = new CountDownLatch(totalThreadAmount);
-		this.addItemsInThreads(number, addingThreadAmount, latch);
-		this.removeItemsInThreads(number, removingThreadAmount, latch);
-		latch.await();
-		MatcherAssert.assertThat(
-			nonThreadSafeList.size(), 
-			CoreMatchers.equalTo(remainingThreadAmount)
-		);
-	}
-
-	private void addItemsInThreads(final EventValue<Integer> number, final int amount, final CountDownLatch outerLatch) {
-		final CountDownLatch latch = new CountDownLatch(1);
-		final List<Thread> threads = new ArrayList<Thread>();
-		final Random random = new Random();
-		for (int x = 0; x < amount; x++) {
-			threads.add(new Thread(() -> {
+			
+			@Override
+			public void update(final Integer value) {
+				incrementedAmount[0] = incrementedAmount[0] + value;
+			}
+			
+			@Override
+			public void addEvent(final UpdateEvent<Integer> event) {
+				incrementedAmount[0] = incrementedAmount[0] + incrementAmount;
+			}
+		});
+		final int threadAmount = 100;
+		final CountDownLatch startingLatch = new CountDownLatch(threadAmount);
+		final CountDownLatch endingLatch = new CountDownLatch(threadAmount);
+		for (int x = 0; x < threadAmount; x++) {
+			new Thread(() -> {
 				try {
-					latch.await();
-					number.update(random.nextInt());
+					startingLatch.countDown();
+					startingLatch.await();
 					number.value();
-					outerLatch.countDown();
-				} catch (final InterruptedException exception) {
-					fail("The thread has been interrupted.", exception);
-				}
-			}));
-		}
-		threads.forEach(thread -> thread.start());
-		latch.countDown();
-	}
-	
-	private void removeItemsInThreads(final EventValue<Integer> number, final int amount, final CountDownLatch outerLatch) {
-		final CountDownLatch latch = new CountDownLatch(1);
-		final List<Thread> threads = new ArrayList<>();
-		for (int x = 0; x < amount; x++) {
-			threads.add(new Thread(() -> {
-				try {
-					latch.await();
+					number.update(incrementAmount);
 					number.addEvent((oldValue, newValue) -> {
-						throw new UnsupportedOperationException(
-							"You are not supposed to use this method."
-						);
+						throw new UnsupportedOperationException("It should not be executed.");
 					});
-					number.value();
-					outerLatch.countDown();
-				} catch (final InterruptedException exception) {
-					fail("The thread has been interrupted.", exception);
+					endingLatch.countDown();
+					// @checkstyle IllegalCatchCheck (1 line)
+				} catch (final Exception exception) {
+					fail("An exception has been thrown while executing the thread.", exception);
 				}
-			}));
+			}).start();
 		}
-		threads.forEach(thread -> thread.start());
-		latch.countDown();
+		endingLatch.await();
+		final int numberOfMethods = 3;
+		MatcherAssert.assertThat(
+			incrementedAmount[0], 
+			CoreMatchers.equalTo(threadAmount * numberOfMethods)
+		);
 	}
 }
