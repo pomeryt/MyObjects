7d24076640368d484fde2c67429c20c3e3f32c3b
Rin <pomeryt@gmail.com>
2018-12-09 23:46:31 +0900

    Add package information. fixes #16

---
+++ src/main/java/plain/value/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * It contains objects that represent a value. <br>
+ * The objects in this package are intended to prevent programmers from re-assigning a value to a existing variable. <br>
+ * Instead, they are supposed ask the object to change its content. <br>
+ * From the object-oriented perspective, re-assigning a value is same as changing the identity of object. <br>
+ * The identity of object should not be changed just like people should not change someone's identity.
+ */
+package plain.value;
---
+++ src/test/java/plain/value/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Test.
+ */
+package plain.value;
