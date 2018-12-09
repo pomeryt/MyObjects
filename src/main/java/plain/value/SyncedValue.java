7ebc8d89984ccd79e35beaf2ab595987df6983d0
Rin <pomeryt@gmail.com>
2018-12-10 01:11:03 +0900

    Use lock object to synchronize. fixes #17

--- src/main/java/plain/value/SyncedValue.java
+++ src/main/java/plain/value/SyncedValue.java
@@ -14,24 +14,26 @@ public final class SyncedValue<T> implements EventValue<T> {
 	
 	@Override
 	public T value() {
-		synchronized (this) {
+		synchronized (this.lock) {
 			return this.origin.value();
 		}
 	}
 
 	@Override
 	public void update(final T value) {
-		synchronized (this) {
+		synchronized (this.lock) {
 			this.origin.update(value);
 		}
 	}
 
 	@Override
 	public void addEvent(final UpdateEvent<T> event) {
-		synchronized (this) {
+		synchronized (this.lock) {
 			this.origin.addEvent(event);
 		}
 	}
 
 	private final EventValue<T> origin;
+	
+	private final Object lock = new Object();
 }
